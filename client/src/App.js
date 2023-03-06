3ac4a3e669c0e0e46fec48c6acb93019a850ca5e
tubi55 <tubi55@nate.com>
2023-03-06 12:26:41 +0900

    리액트 컴포넌트 마운트시 특정 객체값을 서버로 전달후 응답 처리코드 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
+import axios from 'axios';
+import { useEffect, useMemo } from 'react';
+
 function App() {
-	return <div className='App'>hello world2</div>;
+	const item = useMemo(() => ({ name: 'David' }), []);
+
+	useEffect(() => {
+		// /api/send로 item객체를 서버쪽에 전달
+		axios
+			.post('/api/send', item)
+			.then((res) => {
+				//서버쪽에서 응답이 성공적으로 넘어오면 해당 값을 콘솔로 출력
+				console.log(res);
+			})
+			.catch((err) => console.log(err));
+	}, [item]);
+
+	return (
+		<div className='App'>
+			<h1>React</h1>
+		</div>
+	);
 }
 
 export default App;
