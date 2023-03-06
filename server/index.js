09da100a87fe8a7acd1afd205d5b69c816fe3355
tubi55 <tubi55@nate.com>
2023-03-06 14:37:57 +0900

    클라이언트에서 넘어온 데이터 DB에 저장

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
+const { Post } = require('./model/postSchema.js');
 
 //클라이언트에서 보내는 데이터를 전달받도록 설정 (body-parser)
 app.use(express.json());
@@ -30,5 +31,16 @@ app.get('*', (req, res) => {
 //리액트 요청 라우터
 app.post('/api/create', (req, res) => {
 	console.log(req.body);
-	res.json({ success: true });
+
+	//postSchema가 적용된 Post모델 생성함수를 호출해서 데이터를 저장한 인스턴스 생성
+	const PostModel = new Post({
+		title: req.body.title,
+		content: req.body.content,
+	});
+
+	//게시글 인스턴스에서 save메서드 호출하면 DB에 데이터가 저장됨
+	//이때 save메서드는 프로미스 객체를 반환하므로 데이저 저장이 성공하면 then, 그렇지 않으면 catch문 실행
+	PostModel.save()
+		.then(() => res.json({ success: true }))
+		.catch(() => res.json({ success: false }));
 });
