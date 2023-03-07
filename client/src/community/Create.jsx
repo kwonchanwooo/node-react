4c99631a14a025028a7761a1ec2371b3b9815f89
tubi55 <tubi55@nate.com>
2023-03-07 10:39:04 +0900

    글 저장시 성공, 실패 유무에 따라 경고창 출력후 페이지 이동

--- client/src/community/Create.jsx
+++ client/src/community/Create.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { useState } from 'react';
 import Layout from '../common/Layout';
+import { useNavigate } from 'react-router-dom';
 
 function Create() {
+	const navigate = useNavigate();
 	const [Tit, setTit] = useState('');
 	const [Con, setCon] = useState('');
 
@@ -11,7 +13,14 @@ function Create() {
 
 		axios
 			.post('/api/create', item)
-			.then((res) => console.log(res))
+			.then((res) => {
+				if (res.data.success) {
+					alert('글 저장이 완료되었습니다. ');
+					navigate('/list');
+				} else {
+					alert('글 저장에 실패했습니다.');
+				}
+			})
 			.catch((err) => console.log(err));
 	};
 
