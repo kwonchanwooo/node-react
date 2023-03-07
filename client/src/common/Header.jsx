99d0b42482694a0b2b2ee1d6b20d35c094735912
tubi55 <tubi55@nate.com>
2023-03-07 10:06:02 +0900

    메뉴 활성화

--- client/src/common/Header.jsx
+++ client/src/common/Header.jsx
@@ -1,5 +1,28 @@
+import { NavLink, Link } from 'react-router-dom';
+
 function Header() {
-	return <header>Header</header>;
+	const activeStyle = { color: 'hotpink' };
+
+	return (
+		<header>
+			<h1>
+				<Link to='/'>DCODELAB</Link>
+			</h1>
+
+			<ul id='gnb'>
+				<li>
+					<NavLink to='/list' style={({ isActive }) => (isActive ? activeStyle : null)}>
+						Show List
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/create' style={({ isActive }) => (isActive ? activeStyle : null)}>
+						Write Post
+					</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
