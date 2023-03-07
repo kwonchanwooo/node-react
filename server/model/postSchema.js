3e9c2e39fa5d936efccf0aa915816d43c70029c3
tubi55 <tubi55@nate.com>
2023-03-07 11:06:17 +0900

    postSchema에 communityNum 항목 추가하고 counterSchema생성

--- server/model/postSchema.js
+++ server/model/postSchema.js
@@ -3,10 +3,14 @@
 //Schema : 데이터베이스에 저장될 자료형식이나 키값을 강제하는 시스템적인 틀
 
 const mongoose = require('mongoose');
-const postSchema = new mongoose.Schema({
-	title: String,
-	content: String,
-});
+const postSchema = new mongoose.Schema(
+	{
+		title: String,
+		content: String,
+		communityNum: Number,
+	},
+	{ collection: 'Posts' }
+);
 
 const Post = mongoose.model('Post', postSchema);
 module.exports = { Post };
