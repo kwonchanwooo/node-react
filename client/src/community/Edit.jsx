dd6d65284fdb9d491a3f56a500f0bca9401bb459
tubi55 <tubi55@nate.com>
2023-03-08 11:22:17 +0900

    Edit컴포넌트 주석 설명

--- client/src/community/Edit.jsx
+++ client/src/community/Edit.jsx
@@ -9,22 +9,23 @@ function Edit() {
 	const [Title, setTitle] = useState('');
 	const [Content, setContent] = useState('');
 
+	//순서1 - 처음 컴포넌트 마운트시 서버쪽에 데이터 요청후 응답값을 Detail에 옮겨담음
 	useEffect(() => {
 		axios.post('/api/community/detail', params).then((res) => {
 			if (res.data.success) {
-				console.log(res.data.detail);
 				setDetail(res.data.detail);
 			}
 		});
 	}, [params]);
 
+	//순서2 - Detail에 값이 담아지면 Title, Content에 제목, 본문을 분리해서 저장
 	useEffect(() => {
-		console.log(Detail);
 		setTitle(Detail.title);
 		setContent(Detail.content);
 	}, [Detail]);
 
 	return (
+		//input의 value에 Title, Content값을 등록해서 onChange이벤트가 발생할 때마다 상태관리
 		<Layout name={'Post'}>
 			<label htmlFor='tit'>Title</label>
 			<input type='text' id='tit' value={Title || ''} onChange={(e) => setTitle(e.target.value)} />
