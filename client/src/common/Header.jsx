5de8eeedcfb3d1dfa8a360c9f9d0b6b6b8a51f8c
tubi55 <tubi55@nate.com>
2023-03-08 09:43:57 +0900

    header styling

--- client/src/common/Header.jsx
+++ client/src/common/Header.jsx
@@ -1,15 +1,44 @@
 import { NavLink, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderWrap = styled.header`
+	width: 350px;
+	height: 100vh;
+	background: #222;
+	position: fixed;
+	top: 0;
+	left: 0;
+	padding: 50px;
+`;
+
+const Logo = styled.h1`
+	margin-bottom: 40px;
+
+	a {
+		font: 40px/1 'arial';
+		color: #fff;
+	}
+`;
+
+const Gnb = styled.ul`
+	a {
+		display: block;
+		padding: 10px;
+		font: bold 16px/1 'arial';
+		color: #bbb;
+	}
+`;
 
 function Header() {
 	const activeStyle = { color: 'hotpink' };
 
 	return (
-		<header>
-			<h1>
+		<HeaderWrap>
+			<Logo>
 				<Link to='/'>DCODELAB</Link>
-			</h1>
+			</Logo>
 
-			<ul id='gnb'>
+			<Gnb>
 				<li>
 					<NavLink to='/list' style={({ isActive }) => (isActive ? activeStyle : null)}>
 						Show List
@@ -20,8 +49,8 @@ function Header() {
 						Write Post
 					</NavLink>
 				</li>
-			</ul>
-		</header>
+			</Gnb>
+		</HeaderWrap>
 	);
 }
 
