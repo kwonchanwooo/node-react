fe0a52c007b203f1f6a8f4f86caa1fa4f55c402b
tubi55 <tubi55@nate.com>
2023-03-09 11:26:54 +0900

    App컴포넌트 마운트시 firebase로 받은 로그인 상태값에 따라 전역 state값 변경

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,30 @@ import GlobalStyle from './GlobalStyle';
 import Join from './user/Join';
 import Loign from './user/Loign';
 
+import firebase from './firebase';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser, logoutUser } from './redux/userSlice';
+
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((userInfo) => {
+			console.log(userInfo);
+			//컴포넌트 마운트시 firebase로 받은 유저정보가 없으면 (로그인 상태가 아니면)
+			//logout함수를 호출해서 유저정보를 비우는 액션객체 반환하고 해당 액션객체를 dispatch로 리듀서에 전달
+			if (userInfo === null) dispatch(logoutUser());
+			//firebase로 받은 유저정보가 있으면 (로그인 상태이면)
+			//loginUser함수 호출시 인수로 로그인된 정보값을 전달해서 해당 정보가 담긴 액션겍체를 생성후 dispatch로 전달
+			else dispatch(loginUser(userInfo.multiFactor.user));
+		});
+	}, [dispatch]);
+
+	useEffect(() => {
+		//firebase.auth().signOut();
+	}, []);
+
 	return (
 		<>
 			<GlobalStyle />
--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -2,17 +2,15 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const userSlice = createSlice({
 	name: 'user',
-	initialState: { displayName: '', uid: '', accessToken: '' },
+	initialState: { displayName: '', uid: '' },
 	reducers: {
 		loginUser: (state, action) => {
 			state.displayName = action.payload.displayName;
 			state.uid = action.payload.uid;
-			state.accessToken = action.payload.accessToken;
 		},
 		logoutUser: (state) => {
 			state.displayName = '';
 			state.uid = '';
-			state.accessToken = '';
 		},
 	},
 });
