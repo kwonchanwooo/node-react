150cc4076b000624171556a7c4333ac062bf04ca
tubi55 <tubi55@nate.com>
2023-03-09 10:24:07 +0900

    로그인된 유저정보값을 전역으로 담기위해 redux-toolkit세팅

---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+	name: 'user',
+	initialState: { displayName: '', uid: '', accessToken: '' },
+	reducers: {
+		loginUser: (state, action) => {
+			state.displayName = action.payload.displayName;
+			state.uid = action.payload.uid;
+			state.accessToken = action.payload.accessToken;
+		},
+		logoutUser: (state) => {
+			state.displayName = '';
+			state.uid = '';
+			state.accessToken = '';
+		},
+	},
+});
+
+//위의 2개의 전역 state변경함수를 각 컴포넌트에서 호출하기 위해 export
+export const { loginUser, logoutUser } = userSlice.actions;
+//위의 함수로 부터 변경된 전역 객체값을 store에 전달하기 위해 reducer export
+export default userSlice.reducer;
